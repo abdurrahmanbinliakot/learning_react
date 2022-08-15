4b70bba8b89bb15646e56019556ae59c28055fac
abdurrahmanbinliakot <abdurrahmanbinliakot@gmail.com>
2022-08-15 13:18:40 +0600

    JSX

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello World</h1>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const userObj = {
+  fname: "Abdur",
+  lname: "Rahman",
+  link: "https://www.reactjs.org",
+  avatarLink: "https://avatars.githubusercontent.com/u/65095033?v=4",
+};
+
+const fullName = (obj) => obj.fname + " " + obj.lname;
+
+const element = (
+  <div>
+    <h1>Hello, {fullName(userObj)}!</h1>
+    <hr />
+    <img src={userObj.avatarLink} alt={fullName(userObj)}></img>
+    <br/>
+    <a href={userObj.link}>React Link</a>
+  </div>
+);
+root.render(element);
+
 reportWebVitals();
