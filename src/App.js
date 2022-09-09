bd305febc0eb9046965dc6372f1c11d19b8f525f
abdurrahmanbinliakot <abdurrahmanbinliakot@gmail.com>
2022-09-09 19:35:35 +0600

    React useEffect Hook init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-// import logo from './logo.svg';
-import "./App.css";
-// import Task from './Component/Task';
-// import TaskObj from './Component/TaskObj';
-// import Todo from './Component/Todo';
+
 import Counter from "./Component/Counter";
 
 function App() {
   return (
     <div className="App">
-      {/* <Todo/> */}
-      {/* <Task /> */}
-      {/* <TaskObj /> */}
       <Counter />
     </div>
   );
--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,13 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Counter() {
-    const [count, setCount]=useState(0)
-    
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(new Date());
+  const [timeZone ,settimeZone]=useState(false)
+  const [showHidden ,setShowHidden]=useState(true)
+
+  useEffect(() => {
+    document.title = `Clicked ${count} Times`;
+  },[count, timeZone]);
+
+  useEffect(()=>{
+    const interval =setInterval(()=>setTime(new Date()),100)
+    // setInterval(()=>setTime((preTime)=>preTime+1),100)  //nor working
+
+   
+  });
   return (
     <>
-    <div>{count}</div>
-    <button onClick={()=>setCount((prev)=>prev +1)}>Click To incriment </button>
-    <button onClick={()=>setCount((prev)=>prev -1)}>Click To decriment </button>
+    <h2>{showHidden && time.toLocaleTimeString(timeZone? "bn-BD" : "en-US")}</h2>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Click To incriment
+      </button>
+      <button onClick={() => setCount((prev) => prev - 1)}>
+        Click To decriment
+      </button>
+    <br />
+    <br />
+      <button onClick={() => settimeZone((timeZone) => !timeZone )}>
+        {!timeZone? "Go to Bangla Clock" : "Go to English Clock"}  
+      </button>
+      <button onClick={() => setShowHidden((showHidden) => !showHidden )}>
+        {showHidden? "Hide the Clock" : "Show the Clock"}  
+      </button>
     </>
-  )
+  );
 }
