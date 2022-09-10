470dafc940b8f979b3aaf8b79aa0bab717be9088
abdurrahmanbinliakot <abdurrahmanbinliakot@gmail.com>
2022-09-10 08:09:29 +0600

    init react tailwind

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -3,35 +3,38 @@ import React, { useEffect, useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
   const [time, setTime] = useState(new Date());
-  const [timeZone ,settimeZone]=useState(false)
-  const [showHidden ,setShowHidden]=useState(true)
+  const [timeZone, settimeZone] = useState(false);
+  const [showHidden, setShowHidden] = useState(true);
 
   useEffect(() => {
     document.title = `Clicked ${count} Times`;
-  },[count, timeZone]);
+  }, [count, timeZone]);
 
-  useEffect(()=>{
-    const interval =setInterval(()=>setTime(new Date()),100)
+  useEffect(() => {
+    setInterval(() => setTime(new Date()), 100);
     // setInterval(()=>setTime((preTime)=>preTime+1),100)  //nor working
-
-   
   });
   return (
     <>
-    <h2>{showHidden && time.toLocaleTimeString(timeZone? "bn-BD" : "en-US")}</h2>
+      <h2 className="text-cyan-500 ">
+        {showHidden && time.toLocaleTimeString(timeZone ? "bn-BD" : "en-US")}
+      </h2>
       <button onClick={() => setCount((prev) => prev + 1)}>
         Click To incriment
       </button>
       <button onClick={() => setCount((prev) => prev - 1)}>
         Click To decriment
       </button>
-    <br />
-    <br />
-      <button onClick={() => settimeZone((timeZone) => !timeZone )}>
-        {!timeZone? "Go to Bangla Clock" : "Go to English Clock"}  
+      <br />
+      <br />
+      <button
+        className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        onClick={() => settimeZone((timeZone) => !timeZone)}
+      >
+        {!timeZone ? "Go to Bangla Clock" : "Go to English Clock"}
       </button>
-      <button onClick={() => setShowHidden((showHidden) => !showHidden )}>
-        {showHidden? "Hide the Clock" : "Show the Clock"}  
+      <button className="py-2 px-4 bg-rose-500 rounded-md text-white font-semibold hover:bg-rose-600" onClick={() => setShowHidden((showHidden) => !showHidden)}>
+        {showHidden ? "Hide the Clock" : "Show the Clock"}
       </button>
     </>
   );
